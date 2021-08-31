59eaed1458c09d672a8020066b45091fd42342ed
oajameso <jameschanakarn@gmail.com>
2021-08-31 17:11:45 +0700

    code-config

---
+++ src/main/java/th/ac/ku/atm/config/ATMConfig.java
@@ -0,0 +1,28 @@
+package th.ac.ku.atm.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import th.ac.ku.atm.*;
+
+@Configuration
+public class ATMConfig {
+    @Bean
+    public DataSource dataSource() {
+        return new DataSourceFile("customers.txt");
+    }
+
+    @Bean
+    public Bank bank() {
+        return new Bank("My Bank", dataSource());
+    }
+
+    @Bean
+    public ATM atm() {
+        return new ATM(bank());
+    }
+
+    @Bean
+    public AtmUI atmUI() {
+        return new AtmUI(atm());
+    }
+}
